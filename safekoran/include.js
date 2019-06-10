bf3e0f6c08e895edb3bc923637f2966f58dd2303
koranrafly <51521698+koranrafly@users.noreply.github.com>
2019-06-10 17:18:00 +0700

    Create include.js

---
+++ safekoran/include.js
@@ -0,0 +1,3 @@
+var go_url = 'https://infosehatku.club/';
+    var api = 'e5eca07e295f8172c893691bc9a7a0c41e82e634';
+    var shorten_includ = ['drive.google.com', 'youtube.com', 'oload.stream', 'rapidvideo.com', 'mega.nz', 'docs.google.com']; 
